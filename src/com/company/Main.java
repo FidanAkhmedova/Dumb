b2d25258b3fcfa3a92e5a3507ba0519b07ce8082
fidan.ahmadova <FidanAkhmedova>
2023-07-10 11:50:33 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer iOb1 = 100;
+        Integer iOb2 = 100;
+        System.out.println(iOb1 == iOb2);
+
+
+
+for (Level myLevel: Level.values()){
+    System.out.println(myLevel);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*  Integer iOb3 = 120;
+        Integer iOb4 = 120;
+        System.out.println(iOb3 == iOb4);*/
+
+        Integer iOb5 = -127;
+        Integer iOb6 = -127;
+        System.out.println(iOb5 == iOb6);
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars[0]);
+
+        int[] arr = {12,3, 54};
+        System.out.println(arr[0]);
+
+
+    }
+}
